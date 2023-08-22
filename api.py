a5124237a7546dc16efcb90a0bd6496e34e587f2
Arsh <87128542+ArshCypherZ@users.noreply.github.com>
2023-08-22 17:59:26 +0530

    Update api.py

--- api.py
+++ api.py
@@ -2,6 +2,7 @@
 # pip install uvicorn[standard]
 # uvicorn api:app --reload
 
+
 from fastapi import FastAPI
 import tg_bot.modules.sql.antispam_sql as sql1
 import tg_bot.modules.sql.blacklistusers_sql as sql2
